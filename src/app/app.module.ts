458cbb4400add4e7215ad456e77a6a82bd28d863
Omar <omarcientifica@gmail.com>
2022-02-09 00:53:04 -0300

    Componente Habilidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuNavComponent } from './components/menu-nav/menu-nav.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MenuNavComponent
+    MenuNavComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule,
